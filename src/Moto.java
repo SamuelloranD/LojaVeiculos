46bfa2d2ef5355015f586ad40ac2723e657ad2c1
José Neto <josepereiraneto1995@gmail.com>
2024-11-20 00:46:22 -0300

    Class moto adicionada

--- src/Moto.java
+++ src/Moto.java
@@ -1,5 +1,23 @@
 public class Moto extends Veiculo {
-    public Moto(String modelo, int tempo) {
+    private String cilindrada;
+
+    public Moto(String modelo, int tempo, String cilindrada) {
         super(modelo, tempo);
+        this.cilindrada = cilindrada;
+    }
+
+    public String getCilindrada() {
+        return cilindrada;
+    }
+
+    @Override
+    public double calcularValorFinal() {
+        return getTempo() * 50; // Valor por dia para motos: R$ 50
+    }
+
+    @Override
+    public String exibirInformacoes() {
+        return super.exibirInformacoes() +
+               "\nCilindrada: " + getCilindrada();
     }
 }
