36d3d31ef4a26e3cb12efdeea3daf841974f16a7
Mailson <mailsonestudo48@gmail.com>
2024-11-20 23:12:19 -0300

    Adicionando classe de carro

--- src/Carro.java
+++ src/Carro.java
@@ -1,5 +1,23 @@
 public class Carro extends Veiculo {
-    public Carro(String modelo, int tempo) {
+    private int portas;
+
+    public Carro(String modelo, int tempo, int portas) {
         super(modelo, tempo);
+        this.portas = portas;
+    }
+
+    public int getPortas() {
+        return portas;
+    }
+
+    @Override
+    public double calcularValorFinal() {
+        return getTempo() * 100; // Valor por dia para carros: R$ 100
+    }
+
+    @Override
+    public String exibirInformacoes() {
+        return super.exibirInformacoes() +
+                "\nNúmero de portas: " + getPortas();
     }
 }
